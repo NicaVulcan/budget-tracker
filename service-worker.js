2247a5d80abd806aa1007db839789eba71b903f9
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-13 01:53:46 -0500

    tried to make pwa work. Service worker still not registering

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,19 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    './public/index.html',
-    './public/js/index.js',
-    './pubcli/css/styles.css',
-    './public/idb.js'
+    './index.html',
+    './js/index.js',
+    './css/styles.css',
+    './idb.js',
+    './manifest.json',
+    './icons/icon-72x72.png',
+    './icon-96x96.png',
+    './icon-128x128.png',
+    './icon-144x144.png',
+    './icon-152x152.png',
+    './icon-192x192.png',
+    './icon-384x384.png',
+    './icon-512x512.png'
 ];
 
 self.addEventListener('install', function (e) {
